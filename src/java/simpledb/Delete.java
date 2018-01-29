c6236a63ff26361974a9f15153501c050c637b9d
Lincong Li <andrewlincong@gmail.com>
2018-01-28 22:13:29 -0800

    implement Delete OpIterator which is very similar to Insert

--- src/java/simpledb/Delete.java
+++ src/java/simpledb/Delete.java
@@ -10,6 +10,9 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private TransactionId tid;
+    private OpIterator child;
+    private TupleDesc resDesc;
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -21,23 +24,35 @@ public class Delete extends Operator {
      */
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        tid = t;
+        this.child = child;
+        Type [] types = new Type[1];
+        String [] names = new String[1];
+        types[0] = Type.INT_TYPE;
+        resDesc= new TupleDesc(types, names);
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return resDesc;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.open();
+        super.open();
     }
 
     public void close() {
         // some code goes here
+        child.close();
+        super.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        close();
+        open();
     }
 
     /**
@@ -51,18 +66,37 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        int cnt = 0;
+        BufferPool bpool = Database.getBufferPool();
+        while(child.hasNext()){
+            Tuple t = child.next();
+            try {
+                bpool.deleteTuple(tid, t);
+            }catch (IOException e){
+                e.printStackTrace();
+                throw new DbException("IO exception happened while trying to insert a tuple through buffer pool");
+            }
+            cnt++;
+        }
+
+        Tuple ret = new Tuple(resDesc);
+        Field dataField = new IntField(cnt);
+        ret.setField(0, dataField);
+        return ret;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
+        OpIterator[] ret = new OpIterator[1];
+        ret[0] = child;
         return null;
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        assert children.length == 1;
+        child = children[0];
     }
-
 }
